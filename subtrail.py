20424152ca1a838b4fa9b9a1c450684e541a4d09
Marg <marg@marginaldeer.com>
2020-08-28 11:32:57 -0500

    Update subtrail.py

--- subtrail.py
+++ subtrail.py
@@ -1,8 +1,9 @@
 import sys
 import requests
 import argparse
+import socket 
 
-api_key = ""
+api_key = "P1ilAI2OzEPmfgZG5uY8DZmOFNYeWQ0C"
 
 if api_key == '':
     sys.exit('\nPlease edit the script to include an API KEY\n')
@@ -12,7 +13,6 @@ description = "This script is designed to pull down all subdomains for a given d
 epilog = """
 EXAMPLE USAGE:
 An API KEY must be specified within the script!
-
 subtrail.py -d example.com -o example_subdomains.txt
 By default if no output file is specified the results will be saved in subdomains.lst
 """
@@ -23,6 +23,8 @@ parser.add_argument("-d", "--domain", required=True,
                     help="The domain excluding the @ sign (example.com)")
 parser.add_argument("-o", "--outfile", default="subdomains.lst",
                     help="Outputs enumerated users to file you specify.")
+parser.add_argument("-r", "--resolve", action='store_true',
+                    help="Will resolve all subdomains to an IP and add to output")
 args = parser.parse_args()
 
 url = 'https://api.securitytrails.com/v1/domain/' + args.domain + '/subdomains'
@@ -38,7 +40,16 @@ jsonResp = x.json()
 with open(args.outfile, 'w') as file:
     for sub in jsonResp['subdomains']:
         fqdn = sub + '.' + args.domain
-        print(fqdn)
-        file.write(fqdn + '\n')
+        if args.resolve == True:
+            try:
+                ip = socket.gethostbyname(fqdn)
+                print(fqdn + ',' + ip)
+                file.write(fqdn + ',' + ip +'\n')
+            except:
+                print(fqdn + ',')
+                file.write(fqdn + ',' + '\n')
+        else:
+            print(fqdn)
+            file.write(fqdn + '\n')
 
 print('\nResults were written to ' + args.outfile)
